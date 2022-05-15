d6b50d532f0fbd81c15349196af89f4cb7a5fca4
Steerpike <imthekoul@gmail.com>
2022-05-15 19:54:34 +0300

    Number styles for grid added + domFunctions

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -6,20 +6,34 @@ let createBattleShipHeader = container => {
 };
 
 let createLetterLabels = (container, className) => {
-  let letterContainer = document.createElement('div');
+  let lettersContainer = document.createElement('div');
   for (let i = 65; i < 75; i++) {
     let index = document.createElement('p');
     index.classList.add('letter-index');
     index.textContent = String.fromCharCode(i);
-    letterContainer.appendChild(index);
+    lettersContainer.appendChild(index);
   }
-  letterContainer.classList = className;
-  container.appendChild(letterContainer);
+  lettersContainer.classList = className;
+  container.appendChild(lettersContainer);
+};
+
+let createNumberLabels = (container, className) => {
+  let numbersContainer = document.createElement('div');
+  for (let i = 1; i <= 10; i++) {
+    let index = document.createElement('p');
+    index.classList.add('number-index');
+    index.textContent = `${i}`;
+    numbersContainer.appendChild(index);
+  }
+  numbersContainer.classList.add(className);
+  container.appendChild(numbersContainer);
 };
 
 let createLabels = container => {
   createLetterLabels(container, 'top-left-letter-index');
   createLetterLabels(container, 'bottom-left-letter-index');
+  createNumberLabels(container, 'top-number-index');
+  createNumberLabels(container, 'bottom-number-index');
 };
 
 export default () => {
