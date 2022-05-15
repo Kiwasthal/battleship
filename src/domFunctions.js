74d70698d7e541d10ef4635e3018f25a553016fa
Steerpike <imthekoul@gmail.com>
2022-05-15 18:21:46 +0300

    Added index display for boards

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -5,7 +5,25 @@ let createBattleShipHeader = container => {
   container.appendChild(battleShipHeader);
 };
 
+let createLetterLabels = (container, className) => {
+  let letterContainer = document.createElement('div');
+  for (let i = 65; i < 75; i++) {
+    let index = document.createElement('p');
+    index.classList.add('letter-index');
+    index.textContent = String.fromCharCode(i);
+    letterContainer.appendChild(index);
+  }
+  letterContainer.classList = className;
+  container.appendChild(letterContainer);
+};
+
+let createLabels = container => {
+  createLetterLabels(container, 'top-left-letter-index');
+  createLetterLabels(container, 'bottom-left-letter-index');
+};
+
 export default () => {
   let container = document.querySelector('.container');
   createBattleShipHeader(container);
+  createLabels(container);
 };
