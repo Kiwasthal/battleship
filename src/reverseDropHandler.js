21b3bbf7e5e96250e643c11bf7ffe8aea6a67dfa
Steerpike <imthekoul@gmail.com>
2022-05-18 11:37:48 +0300

    Added full reversing ship configuration for UI placement

--- src/reverseDropHandler.js
+++ src/reverseDropHandler.js
@@ -40,15 +40,75 @@ export default {
       if (!nodesAround.slice(2, 3).includes(null));
       nodesAround.slice(2, 3).map(node => node.classList.remove('over'));
     }
-    this.handleReverseCarrier(active, e, nodesAround);
-
+    this.handleReverseShipPlacement(active, e, nodesAround);
     document.querySelector('.reverseShip').classList.remove('scaleNormal');
   },
-  handleReverseCarrier(active, e, nodesAround) {
-    if (nodesAround.includes(null)) return;
+  handleReverseShipPlacement(active, e, nodesAround) {
+    let check = [];
     if (active.classList.contains('carrier')) {
-      nodesAround.map(node => (node.style.background = 'red'));
-      e.target.style.background = 'red';
+      if (nodesAround.includes(null)) return;
+      nodesAround.filter(node => {
+        if (node.classList.contains('taken')) check.push(node);
+      });
+      if (e.target.classList.contains('taken') || check.length > 0) return;
+      nodesAround.map(node => {
+        node.style.background = 'yellow';
+        node.classList.add('carrierPlaced', 'taken');
+      });
+      e.target.style.background = 'yellow';
+      e.target.classList.add('carrierPlaced', 'taken');
+    }
+    if (active.classList.contains('battleship')) {
+      if (nodesAround.slice(1, 4).includes(null)) return;
+      nodesAround.slice(1, 4).filter(node => {
+        if (node.classList.contains('taken')) check.push(node);
+      });
+      if (e.target.classList.contains('taken') || check.length > 0) return;
+      nodesAround.slice(1, 4).map(node => {
+        node.style.background = 'purple';
+        node.classList.add('battleshipPlaced', 'taken');
+      });
+      e.target.style.background = 'purple';
+      e.target.classList.add('battleshipPlaced', 'taken');
+    }
+    if (active.classList.contains('cruiser')) {
+      if (nodesAround.slice(1, 3).includes(null)) return;
+      nodesAround.slice(1, 3).filter(node => {
+        if (node.classList.contains('taken')) check.push(node);
+      });
+      if (e.target.classList.contains('taken') || check.length > 0) return;
+      nodesAround.slice(1, 3).map(node => {
+        node.style.background = 'green';
+        node.classList.add('cruiserPlaced', 'taken');
+      });
+      e.target.style.background = 'green';
+      e.target.classList.add('cruiserPlaced', 'taken');
+    }
+    if (active.classList.contains('submarine')) {
+      if (nodesAround.slice(1, 3).includes(null)) return;
+      nodesAround.slice(1, 3).filter(node => {
+        if (node.classList.contains('taken')) check.push(node);
+      });
+      if (e.target.classList.contains('taken') || check.length > 0) return;
+      nodesAround.slice(1, 3).map(node => {
+        node.style.background = 'blue';
+        node.classList.add('submarinePlaced', 'taken');
+      });
+      e.target.style.background = 'blue';
+      e.target.classList.add('submarinePlaced', 'taken');
+    }
+    if (active.classList.contains('destroyer')) {
+      if (nodesAround.slice(2, 3).includes(null)) return;
+      nodesAround.slice(2, 3).filter(node => {
+        if (node.classList.contains('taken')) check.push(node);
+      });
+      if (e.target.classList.contains('taken') || check.length > 0) return;
+      nodesAround.slice(2, 3).map(node => {
+        node.style.background = 'pink';
+        node.classList.add('destroyerPlaced', 'taken');
+      });
+      e.target.style.background = 'pink';
+      e.target.classList.add('destroyerPlaced', 'taken');
     }
   },
 };
