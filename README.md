f8d4725e98d639295321bdfa207582b79ed256f2
Steerpike <imthekoul@gmail.com>
2022-05-19 14:35:25 +0300

    Updated readME

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # battleship
+
 Replicating battleship strategy game.
+
+<img src="https://img.shields.io/badge/Babel-F9DC3E?style=for-the-badge&logo=babel&logoColor=white" /> <img src="https://img.shields.io/badge/Webpack-8DD6F9?style=for-the-badge&logo=Webpack&logoColor=white"> <img src="https://img.shields.io/badge/eslint-3A33D1?style=for-the-badge&logo=eslint&logoColor=white">
+
+# About
--- src/test/player.test.js
+++ src/test/player.test.js
@@ -1,5 +1,23 @@
 import Player from '../Player';
 
-// const gameModule = (() => {
 
-// })();
+beforeEach(() => {
+    let player = Player(true,false,'Captain')
+    let testBoard = {
+        opponentBoard:[],
+        receiveShot: jest.fn((loc) => {
+            testBoard.opponentBoard[loc] = 'miss'
+            return true
+        }),
+        opponentBoard: jest.fn(() => {
+            return testBoard.opponentBoard
+        }),
+    }
+    const arr = [];
+    it('creates Player with correct name'), () => {
+        expect(player.name).toBe('Captain')
+    }
+    it('expects player no to be AI',) , () => {
+        expect(player.isAi).toBe('false')
+    }
+})
